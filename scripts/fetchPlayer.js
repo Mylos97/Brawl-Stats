2a2941b1324072aff8a5f40b3916973507e7c270
msyb <msyb@itu.dk>
2026-08-09 19:52:34 +0200

    Update endpoint

--- scripts/fetchPlayer.js
+++ scripts/fetchPlayer.js
@@ -1,7 +1,7 @@
 import { displayPlayerData } from "./playerInfoStats.js";
 import { createCharts } from "./playerInfoGraphs.js";
 
-const url = "http://81.88.19.86";
+const url = "https://api.findendag.dk";
 
 window.onload = function() {
   fetchData();
